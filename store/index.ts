7bddc226cf99f68db15533956826a1b1fa660fe4
Julio Aguilar <julio.aguilar9928@gmail.com>
2021-06-23 20:22:03 -0500

    🦉 implement redux and counter slice for testing

---
+++ store/index.ts
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit'
+import counterSlice from './counter'
+
+const store = configureStore({
+  reducer: { counter: counterSlice.reducer },
+})
+
+export const counterActions = counterSlice.actions
+
+export default store
